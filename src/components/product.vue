cf05bde59aac989cc8c5bba2080eda7162dd0afd
YLeo5 <leot.jost@bluewin.ch>
2023-12-20 10:44:17 +0100

    aufgabe

---
+++ src/components/product.vue
@@ -0,0 +1,31 @@
+<script>
+import Rating from "./rating.vue";
+
+export default {
+  props: {
+    data: Object,
+  },
+  components: {
+    Rating,
+  },
+};
+</script>
+<template>
+  <div class="card mb-3" style="max-width: 540px">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="data.imageUrl" class="card-img" alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ data.productName }}</h5>
+          <Rating :initalRating="data.rating" />
+          <p class="card-text">{{ data.description }}</p>
+          <p class="card-text">
+            <small class="text-muted">{{ data.releasedDate }}</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
