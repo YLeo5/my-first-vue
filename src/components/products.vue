7fac8830ddfcaf57a658801da94eca809338901c
YLeo5 <leot.jost@bluewin.ch>
2023-12-21 11:07:04 +0100

    Namen der Tiere dem Geschlecht angepasst

--- src/components/products.vue
+++ src/components/products.vue
@@ -13,7 +13,7 @@ export default {
           imageUrl: "https://loremflickr.com/300/300/Kuh?random=1",
           productName: "Kuh",
           birthDate: "4 Juni, 2014",
-          description: "Boris die Kuh lebt auf einem malerischen Bauernhof im Herzen des ländlichen Idylls. Mit ihrem schwarz-weißen Fell und den sanften Augen ist sie das Maskottchen der Bauernfamilie.",
+          description: "Belinda, die Kuh lebt auf einem malerischen Bauernhof im Herzen des ländlichen Idylls. Mit ihrem schwarz-weißen Fell und den sanften Augen ist sie das Maskottchen der Bauernfamilie.",
           rating: 1,
           weight: "431kg",
           gender: "w"
@@ -33,7 +33,7 @@ export default {
           imageUrl: "https://loremflickr.com/300/300/Esel?random=3",
           productName: "Esel",
           birthDate: "7 Mai, 2020",
-          description: "Dave, der Esel, ist alles andere als gewöhnlich. Man nennt ihn liebevoll Dave der magische Käse Esel, denn in seinem Stall geschehen wunderbare Dinge. Jeden Morgen findet man dort frischen, köstlichen Käse",
+          description: "Burro, der Esel, ist alles andere als gewöhnlich. Man nennt ihn liebevoll Burro der magische Käse Esel, denn in seinem Stall geschehen wunderbare Dinge. Jeden Morgen findet man dort frischen, köstlichen Käse",
           rating: 1,
           weight: "349kg",
           gender: "unbestimmt"
